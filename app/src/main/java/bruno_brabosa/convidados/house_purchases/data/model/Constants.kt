2884ba69c25e31798a7328cd23cecbb95ee086e5
bruno.jose <bruno.jose>
2021-08-11 03:32:51 -0300

    feat: Crete Constants.kt to put constants from data layer

---
+++ app/src/main/java/bruno_brabosa/convidados/house_purchases/data/model/Constants.kt
@@ -0,0 +1,7 @@
+package bruno_brabosa.convidados.house_purchases.data.model
+
+object Constants {
+    const val COLUMN_PRODUCT_NAME = "name"
+    const val COLUMN_PRODUCT_QUANTITY = "quantity"
+    const val PRODUCTS_DATABASE_NAME = "products-database"
+}
